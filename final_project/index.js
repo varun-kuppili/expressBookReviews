4dc6bfd77eb9c319c6b93a7285d53a82bed254da
Varun <kvvarun23@gmail.com>
2023-05-12 22:39:25 -0700

    changes

--- final_project/index.js
+++ final_project/index.js
@@ -5,25 +5,26 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-app.use("/customer/auth/*", function auth(req,res,next){
-    if(req.session.user){
-      next();
-    }else{
-      res.status(401).send("Please login first");
-    }
-  });
+  // authentication middleware
+  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
+  try {
+    const decoded = jwt.verify(token, 'secretkey');
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Unauthorized access' });
+  }
 });
  
 const PORT =5000;
 
-app.use("/customer", customer_routes);
+app.use("/", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,80 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
+const books = require('./booksdb.js');
 const regd_users = express.Router();
 
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+  //write code to check if the username is valid
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => { //returns boolean
+  console.log(users);
+  console.log(username, password);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === username && users[i].password === password) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// middleware to decode the JWT token and set the `user` property on the `req` object
+const decodeToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Authorization header is missing' });
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Token is missing' });
+  }
+  jwt.verify(token, 'secretkey', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token is invalid' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (authenticatedUser(username, password)) {
+    // generate JWT token
+    const token = jwt.sign({ username }, 'secretkey');
+    return res.status(200).json({ message: 'Login successful', token });
+  } else {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put('/auth/review/:isbn', decodeToken, (req, res) => {
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.user.username;
+
+  // check if the user has already reviewed the book
+  let bookIndex = books.findIndex((book) => book.isbn === isbn && book.reviews.find((r) => r.username === username));
+  if (bookIndex === -1) {
+    // add a new review
+    bookIndex = books.findIndex((book) => book.isbn === isbn);
+    if (bookIndex === -1) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    books[bookIndex].reviews.push({ username, review });
+  } else {
+    // modify existing review
+    const userReviewIndex = books[bookIndex].reviews.findIndex((r) => r.username === username);
+    books[bookIndex].reviews[userReviewIndex].review = review;
+  }
+
+  return res.status(200).json({ message: 'Review added/modified successfully' });
 });
 
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+module.exports.authenticated = regd_users
